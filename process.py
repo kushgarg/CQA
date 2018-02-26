a58c15a690c4cd93499663963b85ac6207e7a09c
Kush Garg <gargkush97@gmail.com>
2018-02-26 21:51:45 +0530

    questions, relevant questions and parsed parsed !

--- process.py
+++ process.py
@@ -23,16 +23,24 @@ with open(file,'r') as xml_file:
 tree = ET.parse(fr)'''
 root = xml_file.getroot()
 
-ftext = open('data/train_1.txt', 'w')
+ftext = open('data/RelevantQues.txt', 'w')
 str1 = ""
 for child in root.findall('.//RelQBody'):
     str1 = (str1 + str(child.text).replace('\n', '') + '\n').replace("&lt;","").replace("p&gt;","").replace("/","").replace("&amp;","").replace("#160;","").replace("&quot;","").replace("&nbsp;","").replace("b&gt;","")
 ftext.write(str1)
 ftext.close()
 
-ftext = open('data/train_2.txt', 'w')
+ftext = open('data/MainQues.txt', 'w')
 str1 = ""
 for child in root.findall('.//OrgQBody'):
+    str1 = (str1 + str(child.text) + '\n')
+
+ftext.write(str1)
+ftext.close()
+
+ftext = open('data/Comments.txt', 'w')
+str1 = ""
+for child in root.findall('.//RelCText'):
     str1 = (str1 + str(child.text) + '\n')
 
 ftext.write(str1)
